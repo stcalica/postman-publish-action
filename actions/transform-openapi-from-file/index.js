786e04fd773467c2229bd2ae781723f758e4be23
stcalica <kycalica@gmail.com>
2024-12-16 23:30:55 -0800

    fixed filepath for openapi schema

--- actions/transform-openapi-from-file/index.js
+++ actions/transform-openapi-from-file/index.js
@@ -1,28 +1,28 @@
 const core = require('@actions/core');
 const fs = require('fs');
-var Converter = require('openapi-to-postmanv2')
-const path = process.env.GITHUB_ENV;
+var Converter = require('openapi-to-postmanv2');
 
 async function run() {
-    const openapiSchema = core.getInput('openapi_schema');
-    const outputFilePath = core.getInput('output_file');  
+    try {
+        const schemaPath = core.getInput('openapi_schema_path');
+        const openapiSchema = fs.readFileSync(schemaPath, 'utf8');
 
-    Converter.convert({ type: 'string', data: openapiSchema },
-        {}, (err, conversionResult) => {
-          if (!conversionResult.result) {
-            console.log('Could not convert', conversionResult.reason);
-          }
-          else {
-            if (outputFilePath) {
-                fs.writeFileSync(outputFilePath, JSON.stringify(postmanCollection, null, 2), 'utf8');
-                console.log(`Postman collection written to ${outputFilePath}`);
-                core.setOutput('postman_collection', outputFilePath);
+        Converter.convert({ type: 'string', data: openapiSchema }, {}, (err, result) => {
+            if (!result.result) {
+                core.setFailed(`Conversion failed: ${result.reason}`);
             } else {
-                core.setOutput('postman_collection', JSON.stringify(postmanCollection, null, 2));
+                if (outputFilePath) {
+                    fs.writeFileSync(outputFilePath, JSON.stringify(postmanCollection, null, 2), 'utf8');
+                    console.log(`Postman collection written to ${outputFilePath}`);
+                    core.setOutput('postman_collection', outputFilePath);
+                } else {
+                    core.setOutput('postman_collection', JSON.stringify(postmanCollection, null, 2));
+                }
             }
-          }
-        }
-      );
+        });
+    } catch (error) {
+        core.setFailed(`Error reading or converting schema: ${error.message}`);
+    }
 }
 
 run();
